540d1fcca22c14d0d933b3930df27fc31692bb3e
trithangggggggg <trithang2006vn@gmail.com>
2024-12-16 01:55:25 +0700

    Add files via upload

---
+++ Ss16_Ex3.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+void calculateSum(int a, int b, int *result) {
+    *result = a + b;
+}
+int main() {
+    int num1 = 15, num2 = 25;
+    int sum;
+
+    calculateSum(num1, num2, &sum);
+
+    printf("Tong cua %d va %d la: %d\n", num1, num2, sum);
+
+    return 0;
+}
+
